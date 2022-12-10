3942973de658e8577a514d552029e5142044c450
Sutrahen <emmanuelurieto@gmail.com>
2022-12-09 16:56:49 -0800

    changes

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,16 +3,17 @@
 """
 roman_to_int = __import__('12-roman_to_int').roman_to_int
 
-if type(roman_string) = str:
-    roman_string = "X"
-    print("{} = {}".format(roman_number, roman_to_int(roman_string)))
-    roman_string = "VII"
-    print("{} = {}".format(roman_number, roman_to_int(roman_string)))
-    roman_string = "IX"
-    print("{} = {}".format(roman_number, roman_to_int(roman_string)))
-    roman_string = "LXXXVII"
-    print("{} = {}".format(roman_number, roman_to_int(roman_string)))
-    roman_string = "DCCVII"
-    print("{} = {}".format(roman_number, roman_to_int(roman_string)))
-else:
-    print(0)
+def roman_to_int(roman string):
+    if type(roman_string) = str:
+        roman_string = "X"
+            print("{} = {}".format(roman_number, roman_to_int(roman_string)))
+        roman_string = "VII"
+            print("{} = {}".format(roman_number, roman_to_int(roman_string)))
+        roman_string = "IX"
+            print("{} = {}".format(roman_number, roman_to_int(roman_string)))
+        roman_string = "LXXXVII"
+            print("{} = {}".format(roman_number, roman_to_int(roman_string)))
+        roman_string = "DCCVII"
+            print("{} = {}".format(roman_number, roman_to_int(roman_string)))
+    else:
+        print(0)
