9abfb774d2a114890e39f9a25661e22fd6d1d964
Sutrahen <emmanuelurieto@gmail.com>
2022-12-09 17:01:43 -0800

    more changes

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,14 +6,14 @@ roman_to_int = __import__('12-roman_to_int').roman_to_int
 def roman_to_int(roman string):
     if type(roman_string) = str:
         roman_string = "X"
-            print("{} = {}".format(roman_number, roman_to_int(roman_string)))
+            print("{} = {}".format(roman_number,roman_string))
         roman_string = "VII"
-            print("{} = {}".format(roman_number, roman_to_int(roman_string)))
+            print("{} = {}".format(roman_number, roman_string))
         roman_string = "IX"
-            print("{} = {}".format(roman_number, roman_to_int(roman_string)))
+            print("{} = {}".format(roman_number, roman_string))
         roman_string = "LXXXVII"
-            print("{} = {}".format(roman_number, roman_to_int(roman_string)))
+            print("{} = {}".format(roman_number, roman_string))
         roman_string = "DCCVII"
-            print("{} = {}".format(roman_number, roman_to_int(roman_string)))
+            print("{} = {}".format(roman_number, roman_string))
     else:
         print(0)
